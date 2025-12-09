74c3e06d57c2a2923ccdf4eff84b2f9138df89e8
switcha57 <losurdo57@gmail.com>
2025-12-09 22:18:31 +0100

    removed ugly GoTo use

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ Solutions for Advent of Code 2025 in C#.
 
 | Day | Part 1 | Part 2 | Total |
 | --- | --- | --- | --- |
-| Day 1 | 18,13ms | 1,12ms | 19,25ms |
-| Day 2 | 25,87ms | 87,52ms | 113,38ms |
-| Day 3 | 57,39ms | 11,52ms | 68,91ms |
-| Day 4 | 12,70ms | 957,91ms | 970,61ms |
-| Day 5 | 3,13ms | 4,07ms | 7,20ms |
-| Day 6 | 9,88ms | 12,51ms | 22,38ms |
-| Day 7 | 1,14ms | 2,99ms | 4,13ms |
-| Day 8 | 154,88ms | 191,88ms | 346,76ms |
-| Day 9 | 2,59ms | 290,69ms | 293,28ms |
+| Day 1 | 8,64ms | <1ms | 9,55ms |
+| Day 2 | 24,36ms | 90,61ms | 114,97ms |
+| Day 3 | 44,46ms | 10,01ms | 54,46ms |
+| Day 4 | 2,48ms | 12,16ms | 14,64ms |
+| Day 5 | 3,17ms | 6,44ms | 9,61ms |
+| Day 6 | 12,92ms | 19,75ms | 32,67ms |
+| Day 7 | 1,66ms | 4,74ms | 6,40ms |
+| Day 8 | 181,62ms | 159,00ms | 340,62ms |
+| Day 9 | 1,86ms | 304,13ms | 305,99ms |
--- adventOfcode2025/Day04.cs
+++ adventOfcode2025/Day04.cs
@@ -2,13 +2,14 @@
 
 public class Day04 : ISolver
 {
+    int[] dRow = { -1, -1, -1, 0, 0, 1, 1, 1 };
+    int[] dCol = { -1, 0, 1, -1, 1, -1, 0, 1 };
     private bool SurroundingCells(int row, int col)
     {
         int rows = _board.GetLength(0);
         int cols = _board.GetLength(1);
         int count = 0;
-        int[] dRow = { -1, -1, -1, 0, 0, 1, 1, 1 };
-        int[] dCol = { -1, 0, 1, -1, 1, -1, 0, 1 };
+      
         
         for (int i = 0; i < 8; i++)
         {
@@ -48,30 +49,81 @@ public class Day04 : ISolver
         return ans;
     }
 
+    // public object Part2(string[] input) goto solve
+    // {
+    //     _board = new bool[input.Length,input[0].Length];
+    //     for(int i = 0; i < input.Length; i++)
+    //     for (int j = 0; j< input[i].Length; j++)
+    //     {
+    //         if (input[i][j] == '@') _board[i,j] = true;
+    //         else _board[i,j] = false;
+    //     }
+    //
+    //     int ans = 0;
+    //     Inizia:
+    //     for (int i = 0; i < _board.GetLength(0); i++)
+    //     {
+    //         for (int j = 0; j < _board.GetLength(1); j++)
+    //         {
+    //             if (_board[i, j] && SurroundingCells(i, j))
+    //             {
+    //                 ans++;
+    //                 _board[i, j] = false;
+    //                 goto Inizia;
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
     public object Part2(string[] input)
     {
-        _board = new bool[input.Length,input[0].Length];
-        for(int i = 0; i < input.Length; i++)
-        for (int j = 0; j< input[i].Length; j++)
-        {
-            if (input[i][j] == '@') _board[i,j] = true;
-            else _board[i,j] = false;
-        }
+        _board = new bool[input.Length, input[0].Length];
+        var queue = new Queue<(int row, int col)>();
 
-        int ans = 0;
-        Inizia:
-        for (int i = 0; i < _board.GetLength(0); i++)
+        // Initialize board and queue
+        for (int i = 0; i < input.Length; i++)
         {
-            for (int j = 0; j < _board.GetLength(1); j++)
+            for (int j = 0; j < input[i].Length; j++)
             {
-                if (_board[i, j] && SurroundingCells(i, j))
+                _board[i, j] = input[i][j] == '@';
+                if (_board[i, j])
                 {
-                    ans++;
-                    _board[i, j] = false;
-                    goto Inizia;
+                    queue.Enqueue((i, j));
                 }
             }
         }
+
+        int ans = 0;
+        var processed = new HashSet<(int, int)>();
+
+        while (queue.Count > 0)
+        {
+            var (row, col) = queue.Dequeue();
+
+            if (!_board[row, col] || processed.Contains((row, col)))
+                continue;
+
+            if (SurroundingCells(row, col))
+            {
+                ans++;
+                _board[row, col] = false;
+                processed.Add((row, col));
+
+                // Add all 8 neighbors to queue for potential processing
+                for (int i = 0; i < 8; i++)
+                {
+                    int newRow = row + dRow[i];
+                    int newCol = col + dCol[i];
+                    if (newRow >= 0 && newRow < _board.GetLength(0) &&
+                        newCol >= 0 && newCol < _board.GetLength(1))
+                    {
+                        queue.Enqueue((newRow, newCol));
+                    }
+                }
+            }
+        }
+
         return ans;
     }
+
 }
